d1b97e89b6e24a865eb09540a2c74c8339aa4208
Divyapujari <118610147+Divyapujari@users.noreply.github.com>
2024-11-09 21:38:54 +0530

    Update variables.sh

--- Task1/variables.sh
+++ Task1/variables.sh
@@ -1,1 +1,15 @@
+# Creating Variables
+variable1="Hello"
+variable2="Divya"
 
+# Using Variables
+greet="$variable1, $variable2!"
+echo "$greeting Welcome to the world of Bash scripting!"
+
+# Using variables with buil-in functions
+echo "My current bash path - $BASH"
+echo "Bash version I am using - $BASH_VERSION"
+echo "PID of bash I am running - $$"
+echo "My home directory - $HOME"
+echo "Where am I currently? - $PWD"
+echo "My hostname - $HOSTNAME"
